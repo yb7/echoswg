3e08e3de8772c85df0ecc223b18abe7f1513ed33
wangyb <wyb7@qq.com>
2022-04-15 18:35:03 +0800

    add swagger resource

--- go.mod
+++ go.mod
@@ -1,20 +1,26 @@
 module github.com/yb7/echoswg
 
-go 1.12
+go 1.18
 
 require (
+	github.com/go-playground/locales v0.13.0
 	github.com/go-playground/universal-translator v0.17.0
 	github.com/go-playground/validator/v10 v10.4.1
 	github.com/gorilla/schema v1.1.0
-	github.com/labstack/echo/v4 v4.2.0
-	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/labstack/echo/v4 v4.7.2
 	github.com/olekukonko/tablewriter v0.0.1
-	github.com/stretchr/objx v0.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
-	golang.org/x/net v0.0.0-20210119194325-5f4716e94777 // indirect
-	golang.org/x/sys v0.0.0-20210217090653-ed5674b6da4a // indirect
-	golang.org/x/text v0.3.5 // indirect
-	golang.org/x/tools v0.0.0-20190608022120-eacb66d2a7c3 // indirect
-	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
+)
+
+require (
+	github.com/labstack/gommon v0.3.1 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/net v0.0.0-20220412020605-290c469a71a5 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
---
+++ swaggerui.go
@@ -0,0 +1,71 @@
+package echoswg
+
+import (
+	"embed"
+	"fmt"
+	"html/template"
+
+	"github.com/labstack/echo/v4"
+)
+
+//go:embed swagger-ui-4.10.3
+var SwaggerUiFS embed.FS
+
+type SwaggerConfig struct {
+	UrlPrefix   string
+	Title       string
+	Description string
+	ApiDocUrl   string
+}
+
+func ServeSwagger(e *echo.Echo, config SwaggerConfig) {
+
+	t := template.Must(template.ParseFS(SwaggerUiFS, "swagger-ui-4.10.3/*.go.html"))
+	// t := &Template {
+	//     templates: template.Must(template.ParseGlob("public/views/*.html")),
+	// }
+	// http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	// 	t.ExecuteTemplate(rw, "index.go.html", map[string]string{"title": "Golang Embed 测试"})
+	// })
+	fs := echo.MustSubFS(SwaggerUiFS, "swagger-ui-4.10.3")
+
+	prefixed := func(orignalUrl string) string {
+		if len(config.UrlPrefix) == 0 {
+			return orignalUrl
+		}
+		urlPrefix := config.UrlPrefix
+		if urlPrefix[0:1] != "/" {
+			urlPrefix += "/" + config.UrlPrefix
+		}
+		return urlPrefix + orignalUrl
+	}
+
+	for _, tmpl := range t.Templates() {
+		fmt.Printf("loading template >> %s\n", tmpl.Name())
+	}
+
+	indexHandler := func(c echo.Context) error {
+		apiDocUrl := config.ApiDocUrl
+		if len(config.ApiDocUrl) == 0 {
+			apiDocUrl = prefixed("/swagger/api-docs")
+		}
+		params := map[string]string{"url": apiDocUrl}
+		if err := t.ExecuteTemplate(c.Response().Writer, "index.go.html", params); err != nil {
+			return err
+		}
+		// c.Response().WriteHeader(http.StatusOK)
+		return nil
+	}
+	e.GET(prefixed("/swagger/index.html"), indexHandler)
+	e.GET(prefixed("/swagger/index"), indexHandler)
+	e.GET(prefixed("/swagger/api-docs"), GenApiDoc(config.Title, config.Description))
+	e.GET(prefixed("/swagger"), func(c echo.Context) error {
+		c.Redirect(301, "/swagger/index.html")
+		return nil
+	})
+	e.StaticFS(prefixed("/swagger"), fs)
+}
+
+// func SwaggerUiHandler() http.Handler {
+// 	return http.FileServer(http.FS(swaggerUi))
+// }
